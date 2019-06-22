10b91c4dec4d18de9900e9a3f4dc96d9015d913d
diegoarriaw <49913931+diegoarriaw@users.noreply.github.com>
2019-06-22 00:28:41 -0400

    libnegocio de pedido

--- LibData/LibData/BaseDato.cs
+++ LibData/LibData/BaseDato.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+
 namespace LibData
 {
     public class BaseDato
--- LibNegocio/LibNegocio/Pedido.cs
+++ LibNegocio/LibNegocio/Pedido.cs
@@ -1,10 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
+using LibData;
 
 namespace LibNegocio
 {
     public class Pedido
     {
-    }
-}
+        #region Atributos
+        DateTime fecha;
+        int total;
+        int id_vendedor;
+        int id_cliente;
+        bool estado;
+        #endregion
+
+        #region Propiedades
+        public DateTime Fecha
+        {
+            get
+            {
+                return fecha;
+            }
+
+            set
+            {
+                fecha = value;
+            }
+        }
+
+        public int Total
+        {
+            get
+            {
+                return total;
+            }
+
+            set
+            {
+                total = value;
+            }
+        }
+
+        public int Id_vendedor
+        {
+            get
+            {
+                return id_vendedor;
+            }
+
+            set
+            {
+                id_vendedor = value;
+            }
+        }
+
+        public int Id_cliente
+        {
+            get
+            {
+                return id_cliente;
+            }
+
+            set
+            {
+                id_cliente = value;
+            }
+        }
+
+        public bool Estado
+        {
+            get
+            {
+                return estado;
+            }
+
+            set
+            {
+                estado = value;
+            }
+        }
+
+        public Pedido(DateTime fecha, int total, int id_vendedor, int id_cliente, bool estado)
+        {
+            this.Fecha = fecha;
+            this.Total = total;
+            this.Id_vendedor = id_vendedor;
+            this.Id_cliente = id_cliente;
+            this.Estado = estado;
+        }
+        
+        #endregion
+
+        //metodo ingresar pedido
+        public Pedido ingresarPedido(Pedido objPedido)
+        {
+            BaseDato objDB = new BaseDato();
+            objPedido = objPedido.ingresarPedido(objPedido);
+
+            return objPedido;
+        }//fin metodo ingresar
+
+        //metodo listar pedido
+        public Pedido listarPedido(Pedido objPedido)
+        {
+            BaseDato objDB = new BaseDato();
+            objPedido = objDB.listarPedido(objPedido);
+
+            return objPedido;
+        }//fin metodo listar
+
+        //metodo modificar
+        public Pedido modificarPedido(Pedido objPedido)
+        {
+            BaseDato objDB = new BaseDato();
+            objPedido = objDB.modificarPedido(objPedido);
+
+            return objPedido;
+        }//fin metodo modificar
+        
+        //metodo eliminar
+        public Pedido eliminar(Pedido objPedido)
+        {
+            BaseDato objDB = new BaseDato();
+            objPedido = objDB.eliminarPedido(objPedido);
+
+            return objPedido;
+        }//fin metodo eliminar
+
+    }//fin class
+}//fin namespace
