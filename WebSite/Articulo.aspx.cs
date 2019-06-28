9b2ba7c67ff6a9a52297c6458eaf5d31f3a2b66b
nicom <nicom@MNDEZ>
2019-06-27 20:40:10 -0400

    gg

--- WebSite/Articulo.aspx.cs
+++ WebSite/Articulo.aspx.cs
@@ -3,34 +3,124 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using LibNegocio;
 
 public partial class Articulo : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        mostrar();
     }
 
     protected void btnIngresar_Click(object sender, EventArgs e)
     {
-        LibNegocio.Articulo objArticulo = new LibNegocio.Articulo();
-        objArticulo.Nombre= txtNombre.Text;
-        objArticulo.Descripcion = txtDescripcion.Text;
-        objArticulo.Color = txtColor.Text;
-        objArticulo.Tamaño = txtTamaño.Text;
-        objArticulo.Precio = int.Parse(txtPrecio.Text);
-        objArticulo.Coste_u_mayorista = int.Parse(txtCosto.Text);
-        objArticulo.Unidades = int.Parse(txtUnidades.Text);
+        try
+        {
+            Articulo objArticulo = new Articulo();
 
-        objArticulo = objArticulo.ingresarArticulo(objArticulo);
+            objArticulo.Nombre = txtNombre.Text;
+            objArticulo.Descripcion = txtDescripcion.Text;
+            objArticulo.Color = txtColor.Text;
+            objArticulo.Tamaño = txtTamaño.Text;
+            objArticulo.Precio = int.Parse(txtPrecio.Text);
+            objArticulo.Coste_u_mayorista = int.Parse(txtCosto.Text);
+            objArticulo.Unidades = int.Parse(txtUnidades.Text);
+
+            objArticulo.ingresarArticulo(objArticulo);
+
+            if (objArticulo.Exito == true)
+            {
+                lblMensaje.Text = "INGRESADO CORRECTAMENTE";
+                limpiar();
+                mostrar();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            lblMensaje.Text = "ERROR " + ex;
+        }
     }
 
     protected void btnListar_Click(object sender, EventArgs e)
     {
-        LibNegocio.Articulo objArticulo = new LibNegocio.Articulo();
-        objArticulo = objArticulo.listarArticulo(objArticulo);
-        dgListar.DataSource = objArticulo.Data;
-        dgListar.DataBind();
+        mostrar();
+    }
+
+    public void mostrar()
+    {
+        try
+        {
+            Articulo objArticulo = new Articulo();
+            objArticulo.listarArticulo(objArticulo);
+            dgListar.DataSource = objArticulo.Data;
+            dgListar.DataBind();
+        }
+        catch (Exception ex)
+        {
+            lblMensaje.Text = "ERROR " + ex;
+        }
+    }
+
+    public void limpiar()
+    {
+        txtNombre.Text = "";
+        txtDescripcion.Text = "";
+        txtColor.Text = "";
+        txtTamaño.Text = "";
+        txtPrecio.Text = "";
+        txtCosto.Text = "";
+        txtUnidades.Text = "";
+    }
+
+    protected void btnEliminar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Articulo objArticulo = new Articulo();
+            objArticulo.Id = int.Parse(txtId.Text);
+            objArticulo.eliminarArticulo(objArticulo);
+
+            if (objArticulo.Exito == true)
+            {
+                lblMensaje.Text = "ELIMINADO CORRECTAMENTE";
+            }
+            limpiar();
+            mostrar();
+        }
+        catch (Exception ex)
+        {
+            lblMensaje.Text = "ERROR " + ex;
+        }
+    }
+
+    protected void btnAct_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Articulo objArticulo = new Articulo();
+
+            objArticulo.Id = int.Parse(txtId.Text);
+            objArticulo.Nombre = txtNombre.Text;
+            objArticulo.Descripcion = txtDescripcion.Text;
+            objArticulo.Color = txtColor.Text;
+            objArticulo.Tamaño = txtTamaño.Text;
+            objArticulo.Precio = int.Parse(txtPrecio.Text);
+            objArticulo.Coste_u_mayorista = int.Parse(txtCosto.Text);
+            objArticulo.Unidades = int.Parse(txtUnidades.Text);
+
+            objArticulo.actualizarArticulo(objArticulo);
+
+            if (objArticulo.Exito == true)
+            {
+                lblMensaje.Text = "ACTUALIZADO CORRECTAMENTE";
+                mostrar();
+                limpiar();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            lblMensaje.Text = "ERROR " + ex;
+        }
     }
 }
